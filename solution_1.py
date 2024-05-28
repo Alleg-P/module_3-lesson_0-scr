95f5ade8e83e101858e51b96bd34012ec7c7830f
Alleg <opankov@inbox.ru>
2024-05-28 11:50:16 +0300

    module_3/lesson_0

---
+++ solution_1.py
@@ -0,0 +1,21 @@
+# 
+
+class Animal:
+    def __init__(self, name, sound):
+        self.name = name
+        self.sound = sound
+
+    def make_sound(self):
+        print(f"{self.name} says {self.sound}")
+
+    def eat(self, food):
+        print(f"{self.name} is eating {food}")
+
+# Пример использования
+animal1 = Animal("Dog", "Woof")
+animal1.make_sound()
+animal1.eat("bone")
+
+animal2 = Animal("Cat", "Meow")
+animal2.make_sound()
+animal2.eat("fish")
